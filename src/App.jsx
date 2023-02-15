801a1e6e70f3a6b88b6d85881fd7b7954206d8b3
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-16 09:42:53 +1000

    Add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,36 @@
-export const App = () => 'portfolio'
+import { createContext, useState } from 'react'
+import { Navbar } from './components/Navbar'
+import { Hero } from './components/Hero'
+import { Divider } from './components/utilities/Divider'
+import { About } from './components/About/About'
+import { Projects } from './components/Projects/Projects'
+import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
+import { Menu } from './components/Menu'
+
+export const MenuContext = createContext()
+
+export const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  
+  return (
+    <>
+      <MenuContext.Provider 
+        value={{
+          isMenuOpen,
+          setIsMenuOpen
+        }}
+      >
+        <Navbar />
+        <Menu />
+      </MenuContext.Provider>
+      <Hero />
+      <Divider />
+      <About />
+      <Projects />
+      <Divider />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
