5767518af487e5e45319e0e934e9b75c4d42fcd1
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-16 09:45:46 +1000

    Create data for projects and skills

---
+++ src/constants/projects.js
@@ -0,0 +1,42 @@
+import randomStore from '../assets/random-store.png'
+import cvCreator from '../assets/cv-creator.png'
+import weatherDashboard from '../assets/weather-dashboard.png'
+
+export const projects = [
+  {
+    github: 'https://github.com/thomasmdevelopment/random-store',
+    id: 0,
+    image: randomStore,
+    info: [
+      "'Random Store' is an e-commerce store which features products from the Fake Store API.",
+      'Built with React, this project utilises packages such as Redux Toolkit for global state management and React Router for navigating between the views of various components.',
+      'Users can add and remove products from their shopping cart and track the total cost of their order.',
+    ],
+    title: 'Random Store',
+    website: 'https://thomasmdevelopment.github.io/random-store'
+  },
+  {
+    github: 'https://github.com/thomasmdevelopment/cv-creator',
+    id: 1,
+    image: cvCreator,
+    info: [
+      'React application that allows users to input their details into a CV template which is downloadable in PDF format.',
+      "The template contains information such as work experience, education, skills and more. The colour scheme of the template is customisable.",
+      'To avoid prop drilling, this project uses the Context API to share data across its components.'
+    ],
+    title: 'CV Creator',
+    website: 'https://thomasmdevelopment.github.io/cv-creator'
+  },
+  {
+    github: 'https://github.com/thomasmdevelopment/weather-dashboard',
+    id: 2,
+    image: weatherDashboard,
+    info: [
+      'Built with HTML, CSS and JavaScript, this project gives users access to real-time, location specific weather data from the OpenWeather API.',
+      'Users can simply search for their location at the top of the page and are then presented with a 5-day forcast along with some additional information about the current day.',
+      "The data can be shown in either Celsius or Fahrenheit."
+    ],
+    title: 'Weather Dashboard',
+    website: 'https://thomasmdevelopment.github.io/weather-dashboard'
+  }
+]
---
+++ src/constants/skills.js
@@ -0,0 +1,69 @@
+import reactLogo from '../assets/react.svg'
+import typescriptLogo from '../assets/typescript.svg'
+import reduxLogo from '../assets/redux.svg'
+import styledComponentsLogo from '../assets/styled-components.png'
+import gitLogo from '../assets/git.svg'
+import npmLogo from '../assets/npm.svg'
+import javascriptLogo from '../assets/javascript.svg'
+import cssLogo from '../assets/css.svg'
+import bootstrapLogo from '../assets/bootstrap.svg'
+import htmlLogo from '../assets/html.svg'
+import cLogo from '../assets/c.svg'
+
+export const skills = [
+  {
+    id: 0,
+    name: 'REACT',
+    logo: reactLogo
+  },
+  {
+    id: 1,
+    name: 'TYPESCRIPT',
+    logo: typescriptLogo
+  },
+  {
+    id: 2,
+    name: 'JAVASCRIPT',
+    logo: javascriptLogo
+  },
+  {
+    id: 3,
+    name: 'CSS',
+    logo: cssLogo
+  },
+  {
+    id: 4,
+    name: 'HTML',
+    logo: htmlLogo
+  },
+  {
+    id: 5,
+    name: 'GIT',
+    logo: gitLogo
+  },
+  {
+    id: 6,
+    name: 'REDUX TOOLKIT',
+    logo: reduxLogo
+  },
+  {
+    id: 7,
+    name: 'STYLED-COMPONENTS',
+    logo: styledComponentsLogo
+  },
+  {
+    id: 8,
+    name: 'BOOTSTRAP',
+    logo: bootstrapLogo
+  },
+  {
+    id: 9,
+    name: 'NPM',
+    logo: npmLogo
+  },
+  {
+    id: 10,
+    name: 'C',
+    logo: cLogo
+  },
+]
