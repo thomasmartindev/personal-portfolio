bfa2f84d63e83933398b5364e800e5b34d54c144
Thomas Martin <thomasmdevelopment@gmail.com>
2023-02-16 09:42:14 +1000

    Add imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { ThemeProvider } from 'styled-components'
+import { Theme } from './theme/Theme'
+import { GlobalStyle } from './theme/GlobalStyle'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
