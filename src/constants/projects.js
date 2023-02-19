d65fcc5292f5c8613550c8b36e269b0fd19aac8f
Thomas Martin <thomasmartindev@hotmail.com>
2023-02-20 09:05:51 +1000

    Edit projects section

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -32,8 +32,8 @@ export const projects = [
     id: 2,
     image: weatherDashboard,
     info: [
-      'Built with HTML, CSS and JavaScript, this project gives users access to real-time, location specific weather data from the OpenWeather API.',
-      'Users can simply search for their location at the top of the page and are then presented with a 5-day forcast along with some additional information about the current day.',
+      'Built with HTML, CSS and JavaScript, this project gives users access to real-time, location-specific weather data from the OpenWeather API.',
+      'Users can simply search for their location at the top of the page and are then presented with a 5-day forecast along with some additional information about the current day.',
       "The data can be shown in either Celsius or Fahrenheit."
     ],
     title: 'Weather Dashboard',
